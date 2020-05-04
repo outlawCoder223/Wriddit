6755266b40a2cee4faafe6434ab9cb40cdd29ad0
Rance Campbell <rancewcampbell@gmail.com>
2020-05-04 22:33:36 +0000

    add upvote click functionality

--- public/scripts/story.js
+++ public/scripts/story.js
@@ -12,3 +12,17 @@ $('.merge').on('click', function(event) {
   const story = url.replace('/stories/', '');
   $.post(url + '/contributions/:contribution_id', {story_id: story, contribution_id: contribution});
 });
+
+$('.upvote').click(function(event) {
+  if ($(this).hasClass('on')) {
+    const upvoteCount = $(this).siblings('p')[0].innerHTML;
+    $(this).siblings('p')[0].innerHTML = Number(upvoteCount) - 1;
+    $(this).css('opacity', 0.5);
+    $(this).toggleClass('on');
+  } else {
+    const upvoteCount = $(this).siblings('p')[0].innerHTML;
+    $(this).siblings('p')[0].innerHTML = Number(upvoteCount) + 1;
+    $(this).css('opacity', 1);
+    $(this).toggleClass('on');
+  }
+});
