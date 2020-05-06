f979cbbd811abc1c5df7fad94bd797b7bcf5cf40
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-05-06 19:26:25 +0000

    myStories route complete and view instnatiated

--- queries/users_get_queries.js
+++ queries/users_get_queries.js
@@ -9,15 +9,11 @@ WHERE users.id = $1
 GROUP BY content, title, genres.name, users.name;
 `
 
-const selectStoryByUser = `
-SELECT content, title, genres.name, users.name, to_char(AVG (rating),'9D9') AS average_rating
+const getStoriesByUser = `
+SELECT content, title, users.name, stories.state
 FROM stories
-JOIN story_ratings ON story_ratings.story_id = stories.id
-JOIN story_genres ON story_genres.story_id = stories.id
-JOIN genres ON genre_id = genres.id
 JOIN users ON author_id = users.id
-WHERE users.id = $1 AND title = '$2'
-GROUP BY content, title, genres.name, users.name;
+WHERE users.id = $1
 `
 
 const getUserName = `
@@ -27,6 +23,6 @@ SELECT name FROM users WHERE id = $1;
 
 module.exports = {
   selectStoriesByUser,
-  selectStoryByUser,
-  getUserName
+  getUserName,
+  getStoriesByUser
 };
