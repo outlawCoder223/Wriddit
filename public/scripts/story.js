ff7f13deaf34a8ee36a2449af49b77da8d4e35b4
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-05-07 22:23:04 +0000

    fixing new contribution renderPost function

--- public/scripts/story.js
+++ public/scripts/story.js
@@ -2,7 +2,7 @@
 
 // create new contribution when form submitted
 $(document).ready(() => {
-  $('#new-contribution').on('submit', function(event) {
+  $('#new-contribution').on('submit', function (event) {
     event.preventDefault();
     const url = window.location.pathname;
     const storyId = getStoryId(url);
@@ -12,28 +12,28 @@ $(document).ready(() => {
       content: content,
     };
 
-    $.post('/stories/:story_id/contributions',data)
-      .done(function(res) {
+    $.post('/stories/:story_id/contributions', data)
+      .done(function (res) {
         renderPost(JSON.parse(res));
       });
   });
 
   // merge contribution into story body and erase all previous contributions.
-  $('.merge').on('click', function(event) {
+  $('.merge').on('click', function (event) {
     event.preventDefault();
 
     const contribution = $(this).attr('id');
     const url = window.location.pathname;
     const storyId = getStoryId(url);
 
-    $.post(url + `/${contribution}`, {story_id: storyId, contribution_id: contribution})
-      .then(function() {
+    $.post(url + `/${contribution}`, { story_id: storyId, contribution_id: contribution })
+      .then(function () {
         window.location.reload();
       });
   });
 
   // dummy upvote functionality
-  $('#contribution-container').on('click','.upvote',function(event) {
+  $('#contribution-container').on('click', '.upvote', function (event) {
     event.preventDefault();
     const self = this;
 
@@ -41,18 +41,18 @@ $(document).ready(() => {
     const contId = article.data('id');
 
 
-    $.post('/stories/contributionupvote', {contId})
-      .then(function(res) {
+    $.post('/stories/contributionupvote', { contId })
+      .then(function (res) {
         article.find('.upvote-number').text(res.upvotes);
       });
   });
 
-  $('#mark-complete').click(function(event) {
+  $('#mark-complete').click(function (event) {
     const url = window.location.pathname;
     const storyId = getStoryId(url);
 
-    $.post(`/stories/${storyId}/complete`, {storyId})
-      .then(function() {
+    $.post(`/stories/${storyId}/complete`, { storyId })
+      .then(function () {
         const origin = window.location.origin;
         const path = '/stories/' + storyId;
         window.location.href = origin + path;
@@ -71,15 +71,14 @@ $(document).ready(() => {
   const renderPost = (post) => {
     const newPost = `
     <article class="contribution">
-                <p>${post.content}</p>
+                <p class="div-content">${post.content}</p>
                 <footer>
-                  <div>
-                    <button class="upvote" ><img src="/images/hands-and-gestures.png" alt=""></button>
+                <div class="upvote-box">
+                    <button class="upvote"><img src="/images/hands-and-gestures.png" alt=""></button>
                     <p>${post.upvotes}</p>
                   </div>
-                  <div>
+                  <div class="button-id-and-merge">
                     <h4>${post.name}</h4>
-                      <button id="${post.id}" class="merge">Merge</button>
                   </div>
                 </footer>
             </article>
