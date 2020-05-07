185e8a9f93ebd8ab6c5fa464abc2e62400bb8a2f
Rance Campbell <rancewcampbell@gmail.com>
2020-05-07 21:01:53 +0000

    create browseAllStories

--- routes/stories.js
+++ routes/stories.js
@@ -2,73 +2,13 @@ const express = require('express');
 const router = express.Router();
 const rp = require('request-promise-native');
 
-const {
-  getCompleteStoryById,
-  getIncompleteStoryById,
-  getActiveContributions,
-  getRandomIncompleteStory,
-  getAllUnfinishedStories,
-  getRandomCompleteStory,
-  getAllTopStories
-} = require('../queries/stories_get_queries');
-
-const {
-  createNewStory,
-  markStoryComplete,
-  updateLikesofStory
-} = require('../queries/stories_post_queries');
-
-const {
-  createContribution,
-  renderNewContribution,
-  mergeContribution1,
-  mergeContribution2,
-  getContributionById
-} = require('../queries/contributions_queries');
-
-const { getUserName } = require('../queries/users_get_queries');
-
-const { getStoryByGenreName } = require('../queries/genres_queries');
-
 module.exports = (myDB) => {
   //browse all stories
   router.get('/', (req, res) => {
     const user = req.session.user;
-    const templateVars = { user };
-    const promise1 = myDB.getUsername(user);
-    const promise2 = myDB.getRandomIncomplete(4);
-    const promise3 = myDB.getRandomComplete(3);
-    const promise4 = myDB.getByGenre();
-    Promise.all([promise1, promise2, promise3, promise4])
-      .then((data) => {
-        templateVars.username = data[0].rows[0].name;
-
-        templateVars.incomplete = {
-          first: data[1].rows[0],
-          second: data[1].rows[1],
-          third: data[1].rows[2]
-        };
-        templateVars.complete = {
-          first: data[2].rows[0],
-          second: data[2].rows[1],
-          third: data[2].rows[2]
-        };
-        templateVars.seventh = data[1].rows[3];
-        templateVars.genres = {
-          comedy: data[3].rows[1],
-          romance: data[3].rows[5],
-          action: data[3].rows[0],
-          crime: data[3].rows[2]
-        };
-      })
-      .then(() => {
+    myDB.browseAllStories(user, 4, 3)
+      .then((templateVars) => {
         res.render('stories', templateVars);
-      })
-      .catch(err => {
-        console.log(err);
-        res
-          .status(500)
-          .json({ error: err.message });
       });
   });
 
